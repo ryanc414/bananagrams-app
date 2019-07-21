a224f818f2399f6bc9d56c6e488ac46b7931be85
Ryan Collingham <ryanc118@gmail.com>
2019-07-21 14:41:18 +0100

    Render a basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const size = 10;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid width={size} height={size} />
     </div>
+  );
+}
+
+function Grid(props: {width: number, height: number}) {
+    const grid = (
+      [...Array(props.height)]
+        .map((_, y) => <GridRow width={props.width} y={y} key={y} />
+        )
+      );
+
+    console.log(grid);
+
+    return (
+      <div className="grid">
+        {grid}
+      </div>
+    );
+}
+
+function GridRow(props: {y: number, width: number}) {
+  const grid_row = (
+    [...Array(props.width)]
+      .map((_, x) => <GridElement position={[x, props.y]} key={x} />)
+  );
+
+  return (
+    <div className="grid-row">
+      {grid_row}
+    </div>
+  );
+}
+
+function GridElement(props: {position: Array<number>}) {
+  return (
+    <button className="grid-element" onClick={() => console.log("click")}>
+      {props.position[0]}
+    </button>
   );
 }
 
